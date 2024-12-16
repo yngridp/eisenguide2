0b87997909b4e9120be7e889efa028a4765ebc52
Yngrid Padilha <yngrid_padilha@hotmail.com>
2024-12-15 23:25:07 -0300

    Configuração banco de dados

--- src/main/java/com/eisenguide2/model/Task.java
+++ src/main/java/com/eisenguide2/model/Task.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -13,6 +11,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
@@ -48,7 +47,7 @@ public class Task {
     private String objective;
     
     @ManyToOne
-    @JsonIgnoreProperties("task")
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user; 
     
     @Enumerated(EnumType.STRING)
--- src/main/java/com/eisenguide2/model/User.java
+++ src/main/java/com/eisenguide2/model/User.java
@@ -12,12 +12,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
